f6dbdfd55b81c4efc5a78a5f732af6aff90f8de4
beautifulchair <beautifulchair13@gmail.com>
2022-12-07 19:20:05 +0900

    To give all files

--- insert_json_to_database.py
+++ insert_json_to_database.py
@@ -1,17 +1,22 @@
 import config
 import datebase as db
 import json
+import os
 
-jsonfile_name = 'data_sample.json'
+files = sorted(os.listdir("./data"))
+print("\n".join(files))
+for file_name in files:
+    if (not file_name.endswith('.json')):
+        print(f"data/{file_name} is not json file")
+        continue
+    with open(f"data/{file_name}", 'r') as f:
+        data = json.load(f)
 
-with open(jsonfile_name, 'r') as f:
-    data = json.load(f)
+    db_name = config.db_name
+    host = config.host
+    user = config.user
 
-db_name = config.db_name
-host = config.host
-user = config.user
-
-with db.Datebase(db_name, host, user) as db_sa:
-    db_sa.insert("date", data["date"])
-    db_sa.insert("time", data["time"])
-    db_sa.do()
+    with db.Datebase(db_name, host, user) as db_sa:
+        db_sa.insert("date", data["date"])
+        db_sa.insert("time", data["time"])
+        db_sa.do()
